bae095edb440388086da977ded41706ef5164e3d
Aakash <aakashhkumrr07@gmail.com>
2025-08-25 18:43:08 +0530

    Database connection

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require("express")
+const connectDB = require('./models/connection')
 const app = express()
 
 // console.log(process.env.PORT)
+
+connectDB()
 
 const PORT = process.env.PORT || 4000
 
