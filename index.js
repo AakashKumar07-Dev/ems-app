f77dc11e825e0962f1e11bca30c0cf3215056397
Aakash <aakashhkumrr07@gmail.com>
2025-08-28 17:40:58 +0530

    user added successfully

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 require('dotenv').config()
 const express = require("express")
-const connectDB = require('./models/connection')
+const connectDB = require("./models/connection")
+const engine = require("express-handlebars").engine
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const app = express()
 
 // console.log(process.env.PORT)
 
 connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.engine('handlebars', engine({
+    // extname: 'handlebars' ,
+  handlebars: allowInsecurePrototypeAccess(Handlebars)}
+));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use("/emp" , require("./controllers/routes"))
 
 const PORT = process.env.PORT || 4000
 
